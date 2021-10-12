05e3470d42814aaa1e55d0ea5538c2eeab858094
BrandonNelson-Creator <n.ellyson@hotmail.com>
2021-10-12 09:39:00 -0300

    Making System

--- Program.cs
+++ Program.cs
@@ -10,7 +10,40 @@ namespace ArraysV1._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("cheese");
+            Console.WriteLine("Ammo system");
+            // storing ammo for all
+
+            int weapon = 0; //0 = pistol  5 = bfg
+
+
+            
+            Console.WriteLine("Before Arrays");
+
+            int pistolAmmo = 6; //Declaration + initialization
+            int shotgun = 2;
+            int rocketLauncher = 5;
+
+            //how to show ammo of current?
+
+          if (weapon == 0)
+            {
+                Console.WriteLine("Pistol Ammo " + pistolAmmo);
+            }
+          else if (weapon == 1)
+            {
+                Console.WriteLine("ShotGun Ammo " + shotgun);
+            }
+          else if (weapon == 2)
+            {
+                Console.WriteLine("Rocket Launcher " + rocketLauncher);
+            }
+                
+
+
+            Console.WriteLine("Arrays");
+
+
+
             Console.ReadKey(true);
         }
     }
